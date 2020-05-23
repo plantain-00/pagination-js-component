f66bee38f331c4465513fbec698ca480c8442b0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-23 00:48:13 +0000

    Deploy plantain-00/pagination-js-component to github.com/plantain-00/pagination-js-component.git:gh-pages

--- packages/react/demo/index.tsx
+++ packages/react/demo/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Pagination } from '../dist/'
 
-class Main extends React.Component<{}, {}> {
+class Main extends React.Component<unknown, unknown> {
   private total = 12
   private current1 = 1
   private count = 2
