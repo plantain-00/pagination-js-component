f4212f9577d364cf2dfb7727140a84b7a824358a
York Yao <me@yorkyao.com>
2020-05-31 16:17:08 +0800

    chore: clean code

--- rev-static.config.ts
+++ rev-static.config.ts
@@ -1,10 +1,11 @@
-module.exports = {
+import { ConfigData } from 'rev-static'
+
+export default {
   inputFiles: [
     'packages/@(vue|react)/demo/**/*.bundle.js',
     'packages/@(vue|react)/demo/**/*.ejs.html',
     'packages/core/demo/*.bundle.css'
   ],
-  json: false,
   outputFiles: file => file.replace('.ejs', ''),
   ejsOptions: {
     rmWhitespace: true
@@ -13,4 +14,4 @@ module.exports = {
   customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
   base: 'packages',
   fileSize: 'file-size.json'
-}
+} as ConfigData
