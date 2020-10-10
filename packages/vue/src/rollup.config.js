ed32a15bf18957f42378086e02f6406ab8d1ebee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-10 22:54:40 +0000

    Deploy plantain-00/pagination-js-component to github.com/plantain-00/pagination-js-component.git:gh-pages

--- packages/vue/src/rollup.config.js
+++ packages/vue/src/rollup.config.js
@@ -12,13 +12,9 @@ export default {
   output: {
     name: 'Pagination',
     file: 'packages/vue/dist/pagination-vue-component.min.js',
-    format: 'umd',
-    globals: {
-      'vue-class-component': 'VueClassComponent'
-    }
+    format: 'umd'
   },
   external: [
-    'vue',
-    'vue-class-component'
+    'vue'
   ]
 }
